04d25c35184c1e0ac0f53936d1969bd332d855b8
Lupercal13 <hpayne13@gmail.com>
2023-02-15 15:31:56 +0900

    update projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
-import placeholder from '../assets/placeholder.png'
+import placeholder from "../assets/placeholder.png";
+import reactTodo from "../assets/react-todo.png";
+import twitchClone from "../assets/twitch-clone.png";
+import nextBlog from "../assets/nextjs-blog.png";
 
 const Projects = () => {
   return (
     <div id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
-      <h1 className="text-4xl font-bold text-center text-[#001b5e]">Projects</h1>
+      <h1 className="text-4xl font-bold text-center text-[#001b5e]">
+        Projects
+      </h1>
       <p className="text-center py-8">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui praesentium
         sed soluta repellendus cupiditate voluptate fuga voluptatibus ipsam
@@ -13,10 +18,10 @@ const Projects = () => {
         totam laudantium.
       </p>
       <div className="grid sm:grid-cols-2 gap-12">
-        <ProjectItem img={placeholder} title='example app' />
-        <ProjectItem img={placeholder} title='example app'/>
-        <ProjectItem img={placeholder} title='example app' />
-        <ProjectItem img={placeholder} title='example app' />
+        <ProjectItem img={twitchClone} title="Twitch clone app" />
+        <ProjectItem img={reactTodo} title="Todo app with theming" />
+        <ProjectItem img={nextBlog} title="NextJS Blog" />
+        <ProjectItem img={placeholder} title="example app" />
       </div>
     </div>
   );
